cdf1f26afc102d36e3292b48f08f18307b009a29
Tolieck <tolieck89@gmail.com>
2024-11-17 23:27:01 +0200

    First one

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins_black: ['Poppins Black', 'serif'], 
+        poppins_bold: ['Poppins Bold', 'serif'], 
+        poppins_regular: ['Poppins-Regular"', 'serif'], 
+        poppins_medium: ['Poppins Medium"', 'serif'], 
+      },
+    
+  },
+  },
+  plugins: [],
+}
+
