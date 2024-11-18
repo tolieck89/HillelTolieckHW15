2e1f20d72ad60a4859aa82e7bd55cf4cc0a23a20
Tolieck <tolieck89@gmail.com>
2024-11-19 01:01:07 +0200

    Third part

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./**/*.{html,js}",
+    "!./node_modules/**/*.*"
+  ],
   theme: {
     extend: {
       fontFamily: {
-        poppins_black: ['Poppins Black', 'serif'], 
         poppins_bold: ['Poppins Bold', 'serif'], 
         poppins_regular: ['Poppins-Regular"', 'serif'], 
         poppins_medium: ['Poppins Medium"', 'serif'], 
+        poppins_light: ['Poppins Light', 'serif'], 
+        poppins_extrabold: ['Poppins ExtraBold', 'serif'], 
       },
 
       colors: {
         'custom_gray': '#C2C8DA',
+        'custom_yellow': '#E6C744',
       },
       borderRadius: {
         'radius_60px': '3.75rem',
-      }
+        
+      },
+    
+
 
     
   },
