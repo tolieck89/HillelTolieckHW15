c9980c127883187a55c26af731685a61ff9775c6
Tolieck <tolieck89@gmail.com>
2024-11-20 23:25:05 +0200

    Third day

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,41 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins_bold: ['Poppins Bold', 'serif'], 
-        poppins_regular: ['Poppins-Regular"', 'serif'], 
-        poppins_medium: ['Poppins Medium"', 'serif'], 
-        poppins_light: ['Poppins Light', 'serif'], 
-        poppins_extrabold: ['Poppins ExtraBold', 'serif'], 
+        poppins_bold: ['Poppins Bold'], 
+        poppins_regular: ['Poppins-Regular'], 
+        poppins_medium: ['Poppins Medium'], 
+        poppins_light: ['Poppins Light'], 
+        poppins_extrabold: ['Poppins ExtraBold'], 
       },
 
       colors: {
         'custom_gray': '#C2C8DA',
         'custom_yellow': '#E6C744',
+        'custom_gray_2': '#7C7C7C',
       },
       borderRadius: {
         'radius_60px': '3.75rem',
         
       },
+      height: {
+        'custom-44rem': '44rem',
+        'custom-4.25rem': '4.25rem',
+        'custom-36rem' : '36rem',
+      },
+      fontSize: {
+        'custom_112':'7rem',
+        'custom_32':'3.25rem',
+      },
+
+      width: {
+        'w-31.5':'31.5rem',
+      },
+
+      margin: {
+        'custom_120px': '7.5rem',
+      },
+
+
     
 
 
